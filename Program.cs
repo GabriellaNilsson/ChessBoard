758fb82fee98e434c1c732e528b56f90301839c5
gabri <gabri@DESKTOP-U2VKQFP>
2023-09-22 16:56:18 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Data;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ChessBoard
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            Console.WriteLine("Enter a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            // for loop for rows
+            for (int x = 0; x < num; x++)
+            {
+                // for loop for columms
+                for (int y = 0; y < num; y++)
+                {
+                    // if statement for rows
+                    if ((x + y) % 2 == 0)
+                    {
+                        // Black squares
+                        Console.Write("◼");
+                    }
+
+                    // if statement for columms
+                    else 
+                    {
+                        // White squares
+                        Console.Write("◻");
+                    }
+                }
+                
+                // inserted line break to start a new row
+                Console.WriteLine();
+                
+            }
+        }
+    }
+}
